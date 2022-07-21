04653760b16bcda1a014725507f13f845435f034
vasyl-makohin <makohin.v@gmail.com>
2022-07-22 01:03:48 +0300

    feat: solve 04-date-tests

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -1,108 +1,130 @@
-/* *******************************************************************************************
- *                                                                                           *
- * Please read the following tutorial before implementing tasks:                              *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Numbers_and_dates#Date_object
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date     *
- *                                                                                           *
- ******************************************************************************************* */
+const assert = require('assert');
+const tasks = require('../src/04-date-tasks');
+it.optional = require('../extensions/it-optional');
+
+describe('04-date-tasks', () => {
+  it.optional('parseDataFromRfc2822 should parse rfc2822 string into a date value', () => {
+    assert.equal(
+      tasks.parseDataFromRfc2822('December 17, 1995 03:24:00').valueOf(),
+      new Date(1995, 11, 17, 3, 24, 0).valueOf(),
+    );
+
+    assert.equal(
+      tasks.parseDataFromRfc2822('Tue, 26 Jan 2016 13:48:02 GMT').valueOf(),
+      1453816082000,
+    );
+
+    assert.equal(
+      tasks.parseDataFromRfc2822('Sun, 17 May 1998 03:00:00 GMT+0100').valueOf(),
+      895370400000,
+    );
+  });
 
 
-/**
- * Parses a rfc2822 string date representation into date value
- * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
- *
- * @param {string} value
- * @return {date}
- *
- * @example:
- *    'December 17, 1995 03:24:00'    => Date()
- *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
- *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
- */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
-}
+  it.optional('parseDataFromIso8601 should parse ISO 8601 string into a date value', () => {
+    assert.equal(
+      tasks.parseDataFromIso8601('2016-01-19T16:07:37+00:00').valueOf(),
+      1453219657000,
+    );
 
-/**
- * Parses an ISO 8601 string date representation into date value
- * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
- *
- * @param {string} value
- * @return {date}
- *
- * @example :
- *    '2016-01-19T16:07:37+00:00'    => Date()
- *    '2016-01-19T08:07:37Z' => Date()
- */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
-}
+    assert.equal(
+      tasks.parseDataFromIso8601('2016-01-19T08:07:37Z').valueOf(),
+      1453190857000,
+    );
+  });
 
 
-/**
- * Returns true if specified date is leap year and false otherwise
- * Please find algorithm here: https://en.wikipedia.org/wiki/Leap_year#Algorithm
- *
- * @param {date} date
- * @return {bool}
- *
- * @example :
- *    Date(1900,1,1)    => false
- *    Date(2000,1,1)    => true
- *    Date(2001,1,1)    => false
- *    Date(2012,1,1)    => true
- *    Date(2015,1,1)    => false
- */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
-}
+  it.optional('isLeapYear should true if specified year is leap', () => {
+    [
+      new Date(2000, 1, 1),
+      new Date(2012, 1, 1),
+    ].forEach((date) => {
+      assert(
+        tasks.isLeapYear(date) === true,
+        `${date} is a leap year`,
+      );
+    });
+
+    [
+      new Date(1900, 1, 1),
+      new Date(2001, 1, 1),
+    ].forEach((date) => {
+      assert(
+        tasks.isLeapYear(date) === false,
+        `${date} is not a leap year`,
+      );
+    });
+  });
 
 
-/**
- * Returns the string representation of the timespan between two dates.
- * The format of output string is "HH:mm:ss.sss"
- *
- * @param {date} startDate
- * @param {date} endDate
- * @return {string}
- *
- * @example:
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,11,0,0)   => "01:00:00.000"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,30,0)       => "00:30:00.000"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,20)        => "00:00:20.000"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
- */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
-}
+  it.optional('timeSpanToString should return the string represation of time span between two dates', () => {
+    [
+      {
+        startDate: new Date(2000, 1, 1, 10, 0, 0),
+        endDate: new Date(2000, 1, 1, 11, 0, 0),
+        expected: '01:00:00.000',
+      }, {
+        startDate: new Date(2000, 1, 1, 10, 0, 0),
+        endDate: new Date(2000, 1, 1, 10, 30, 0),
+        expected: '00:30:00.000',
+      }, {
+        startDate: new Date(2000, 1, 1, 10, 0, 0),
+        endDate: new Date(2000, 1, 1, 10, 0, 20),
+        expected: '00:00:20.000',
+      }, {
+        startDate: new Date(2000, 1, 1, 10, 0, 0),
+        endDate: new Date(2000, 1, 1, 10, 0, 0, 250),
+        expected: '00:00:00.250',
+      }, {
+        startDate: new Date(2000, 1, 1, 10, 0, 0),
+        endDate: new Date(2000, 1, 1, 15, 20, 10, 453),
+        expected: '05:20:10.453',
+      },
+    ].forEach((data) => {
+      assert.equal(
+        tasks.timeSpanToString(data.startDate, data.endDate),
+        data.expected,
+      );
+    });
+  });
 
 
-/**
- * Returns the angle (in radians) between the hands of an analog clock
- * for the specified Greenwich time.
- * If you have problem with solution please read: https://en.wikipedia.org/wiki/Clock_angle_problem
- *
- * SMALL TIP: convert to radians just once, before return in order to not lost precision
- *
- * @param {date} date
- * @return {number}
- *
- * @example:
- *    Date.UTC(2016,2,5, 0, 0) => 0
- *    Date.UTC(2016,3,5, 3, 0) => Math.PI/2
- *    Date.UTC(2016,3,5,18, 0) => Math.PI
- *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
- */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
-}
-
-
-module.exports = {
-  parseDataFromRfc2822,
-  parseDataFromIso8601,
-  isLeapYear,
-  timeSpanToString,
-  angleBetweenClockHands,
-};
+  it.optional('angleBetweenClockHands should returns the angle bettween clock hands for specified Greenwich datetime', () => {
+    [
+      {
+        date: Date.UTC(2016, 3, 5, 0, 0),
+        expected: 0, // 0 deg
+      }, {
+        date: Date.UTC(2016, 3, 5, 3, 0),
+        expected: Math.PI / 2, // 90 deg
+      }, {
+        date: Date.UTC(2016, 3, 5, 15, 0),
+        expected: Math.PI / 2, // 90 deg
+      }, {
+        date: Date.UTC(2016, 3, 5, 6, 0),
+        expected: Math.PI, // 180 deg
+      }, {
+        date: Date.UTC(2016, 3, 5, 18, 0),
+        expected: Math.PI, // 180 deg
+      }, {
+        date: Date.UTC(2016, 3, 5, 9, 0),
+        expected: Math.PI / 2, // 90 deg
+      }, {
+        date: Date.UTC(2016, 3, 5, 21, 0),
+        expected: Math.PI / 2, // 90 deg
+      }, {
+        date: Date.UTC(2016, 3, 5, 14, 20),
+        expected: 0.8726646259971648, // 50 deg
+      }, {
+        date: Date.UTC(2016, 3, 5, 23, 55),
+        expected: 0.4799655442984406, // 27.5 deg
+      },
+    ].forEach((data) => {
+      assert.equal(
+        tasks.angleBetweenClockHands(new Date(data.date)),
+        data.expected,
+        `Incorrect result for angleBetweenClockHands(${new Date(data.date).toUTCString()}):`,
+      );
+    });
+  });
+});
